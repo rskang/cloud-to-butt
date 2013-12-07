588dfa1d7a96985ef3c936065565c71a0837e94e
Rich Kang <rich@saekang.co.uk>
2013-12-07 15:41:42 +0000

    Drumcode to Bumcode!

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bDrumcode\b/g, "Bumcode");
+	v = v.replace(/\bdrumcode\b/g, "Bumcode");
+	v = v.replace(/\bDRUMCODE\b/g, "BUMCODE");
 	
 	textNode.nodeValue = v;
 }
